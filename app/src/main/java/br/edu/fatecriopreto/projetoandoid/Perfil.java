d59aead90cc083b76b75fc7e0a7957f114c17836
Tirys <joanjezzgames@gmail.com>
2015-06-01 21:56:58 -0300

    Botão voltar

--- app/src/main/java/br/edu/fatecriopreto/projetoandoid/Perfil.java
+++ app/src/main/java/br/edu/fatecriopreto/projetoandoid/Perfil.java
@@ -2,6 +2,7 @@ package br.edu.fatecriopreto.projetoandoid;
 
 import android.app.Activity;
 import android.app.Notification;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -10,14 +11,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Perfil extends ActionBarActivity {
+
+    ImageView imgback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.perfil);
+
+        imgback = (ImageView)findViewById(R.id.imgbackperfil);
+
+        imgback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Intent voltar = new Intent(Perfil.this,Main.class);
+             //   startActivityForResult(voltar,1);
+               // overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+                Perfil.this.finish();
+
+            }
+        });
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
