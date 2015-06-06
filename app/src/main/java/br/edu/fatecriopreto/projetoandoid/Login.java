bb414a9db10151d098e04ec41bbf427e1ca977d2
Tirys <joanjezzgames@gmail.com>
2015-06-06 20:42:39 -0300

    Verifica se o usuário está na internet

--- app/src/main/java/br/edu/fatecriopreto/projetoandoid/Login.java
+++ app/src/main/java/br/edu/fatecriopreto/projetoandoid/Login.java
@@ -57,15 +57,20 @@ public class Login extends ActionBarActivity {
         ConnectivityManager connectivity = (ConnectivityManager)
         context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        //if (connectivity != null) {
-            //NetworkInfo netInfo = connectivity.getActiveNetworkInfo();
+        if (connectivity != null) {
+            NetworkInfo netInfo = connectivity.getActiveNetworkInfo();
 
             // Se não existe nenhum tipo de conexão retorna false
-            //if (netInfo != null) {
-                //Toast toast = Toast.makeText(Login.this,"Post excluído com sucesso!",Toast.LENGTH_LONG);
-                //toast.show();
-           // }
-        //}
+            if (netInfo == null) {
+                Toast toast = Toast.makeText(Login.this,"Sem conexão",Toast.LENGTH_LONG);
+                toast.show();
+
+           }
+            else
+            {
+
+
+
 
         ImgLogo = (ImageView) findViewById(R.id.ImgLogo);
 
@@ -175,7 +180,8 @@ public class Login extends ActionBarActivity {
             }
         });
     }
-
+        }
+    }
 
 
 
